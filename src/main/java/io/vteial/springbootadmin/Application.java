0642d12e153c1db969fe31f9d9cef30280d1b8cb
vteial <vteial@gmail.com>
2014-11-18 16:14:10 +0530

    updating latest changes...

---
+++ src/main/java/io/vteial/springbootadmin/Application.java
@@ -0,0 +1,18 @@
+package io.vteial.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+// @Configuration
+// @ComponentScan
+// @EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
